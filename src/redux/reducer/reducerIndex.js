607a419472e9adeea476c33b7a546a5497e7239c
Sam Lijo <samuelvillalvalijo@gmail.com>
2021-03-11 16:06:21 -0600

    reducer index updated

--- src/redux/reducer/reducerIndex.js
+++ src/redux/reducer/reducerIndex.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import loginModeReducer from './loginModeReducer';
 import userReducer from './userReducer';
+import errors from './errorsReducer';
 
 const rootReducer = combineReducers({
-    loginModeReducer(),
-    userReducer(),
+    errors,
+    loginModeReducer,
+    userReducer,
 })
 
 export default rootReducer;
